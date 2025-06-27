76eb1889b231c8c0b4e8b9ee305209c4ff19c5e9
root <root@MITALI.localdomain>
2025-06-27 14:12:08 +0000

    Fix: flake8 style errors

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 
-
 app = Flask(__name__)
 
 
@@ -8,6 +7,6 @@ app = Flask(__name__)
 def hello():
     return "Hello from DevOps CI/CD Pipeline!"
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
